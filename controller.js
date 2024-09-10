b0b6a7f9c1786afc4f40b648c89d76590b20799d
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2024-09-10 12:42:31 +0200

    x

--- controller.js
+++ controller.js
@@ -1,3 +1,46 @@
+
+//Robin
 function hilsKompis(int) {
 
 }
+
+
+//Mariama
+function buyExpensiveUpgrade() {
+    const expensiveUpgradeCost = 70;
+  }
+  //Mariama
+  function buyMediumUpgrade() {
+    const expensiveUpgradeCost = 50;
+  }
+  //Mariama
+  function buyCheapUpgrade() {
+    const expensiveUpgradeCost = 20;
+  }
+  
+
+  
+  let kulOMeter = 0;
+
+  function updateKulOMeter(increase) {
+          
+          kulOMeter += increase;
+      
+          if (kulOMeter < 0) kulOMeter = 0;
+          if (kulOMeter > 100) kulOMeter = 100;
+
+  }
+  
+
+          //LEO
+          function checkWinCon(){
+
+          }
+          //LEO
+          function updateMoney(int){
+  
+          }
+          //LEO
+          function updateKulometer() {
+  
+          }
