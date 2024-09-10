84ca77a9fef3dbc856e8911c8f0c941da428f80c
Stian Finjord <stian.finjord@gmail.com>
2024-09-10 12:55:13 +0200

    Flyttet fra html til model.js

--- model.js
+++ model.js
@@ -1,3 +1,11 @@
+let kulometer;
+let penger;
+let kompisArray = [];
+let kompisObject = {};
+let mekanikerObject = {};
+let mekanikere = [];
+let pupUpMessage = "";        
+        
         //Robin
         function getRandomKompis() {
 
