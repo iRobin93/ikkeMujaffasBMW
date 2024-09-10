3bf2b9c597d5ab285404ceeff4c3cf268c8f1e61
Stian Finjord <stian.finjord@gmail.com>
2024-09-10 12:53:39 +0200

    Viser progress bar

--- view.js
+++ view.js
@@ -2,6 +2,19 @@ showView();
 function showView() {
     document.getElementById("app").innerHTML = /*HTML*/ `
         <div>Dette er main view</div>
-        <div></div>
+       
+        <div> ${drawBar(50,"blue")}</div>
     `
+}
+
+
+function drawBar(percentageFilled, color) {
+    let barColor = 'lightblue';
+ 
+    let html = `
+        <div class="barContainer">
+            <div class="barContent" style="width: ${percentageFilled}%; background-color: ${barColor}"></div>
+        </div>
+    `;
+    return html;
 }
