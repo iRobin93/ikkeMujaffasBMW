2d6afd56219f75372e0de1144753a8d8aec262bb
touraymk <131713898+touraymk@users.noreply.github.com>
2024-09-10 13:41:59 +0200

    Lagt til if statements for kjøp av upgrade

--- controller.js
+++ controller.js
@@ -1,43 +1,44 @@
-
 //Robin
 function hilsKompis(kompisName, moneyIncrease) {
-showKompisView(kompisName, moneyIncrease);
+  showKompisView(kompisName, moneyIncrease);
 }
-
 
 //Mariama
 function buyExpensiveUpgrade() {
-    const expensiveUpgradeCost = 70;
+  const expensiveUpgradeCost = 70;
+  if (penger >= expensiveUpgradeCost) {
+    penger -= expensiveUpgradeCost;
+  } else {
+    alert("Det koster å være storkar, og det er ikke du!");
   }
-  //Mariama
-  function buyMediumUpgrade() {
-    const expensiveUpgradeCost = 50;
+}
+//Mariama
+function buyMediumUpgrade() {
+  const expensiveUpgradeCost = 50;
+  if (penger >= expensiveUpgradeCost) {
+    penger -= expensiveUpgradeCost;
+  } else {
+    alert("Du trenger mer penger, prøv noe billigere!");
   }
-  //Mariama
-  function buyCheapUpgrade() {
-    const expensiveUpgradeCost = 20;
+}
+//Mariama
+function buyCheapUpgrade() {
+  const expensiveUpgradeCost = 20;
+  if (penger >= expensiveUpgradeCost) {
+    penger -= expensiveUpgradeCost;
+  } else {
+    alert("Skaff det no cash!");
   }
-  
+}
 
-  
-
-
-          //LEO
-          function checkWinCon(){
-
-          }
-          //LEO
-          function updateMoney(int){
-  
-          }
-          //LEO
-          function updateKulometer() {
-  
-          }
-
+//LEO
+function checkWinCon() {}
+//LEO
+function updateMoney(int) {}
+//LEO
+function updateKulometer() {}
 
 // Leo Kul-O-Meter
-
 
 function updateKulOMeter(increase) {
   kulOMeter += increase;
@@ -45,13 +46,11 @@ function updateKulOMeter(increase) {
   if (kulOMeter < 0) kulOMeter = 0;
   if (kulOMeter > maxKulOMeter) kulOMeter = maxKulOMeter;
 
+  let kulOPercentage = (kulOMeter / maxKulOMeter) * 100;
 
-let kulOPercentage = (kulOMeter / maxKulOMeter) * 100;
-  
-  document.getElementById('kulOMeterFill').style.width = kulOPercentage + '%';
-  document.getElementById('kulOValue').innerText = kulOMeter + ' points';
+  document.getElementById("kulOMeterFill").style.width = kulOPercentage + "%";
+  document.getElementById("kulOValue").innerText = kulOMeter + " points";
 }
-
 
 // penger
 // Starting money
@@ -61,5 +60,5 @@ function updateMoney(amount) {
 
   if (penger < 0) penger = 0;
 
-  document.getElementById('moneyValue').innerText = penger + ' kr';
+  document.getElementById("moneyValue").innerText = penger + " kr";
 }
