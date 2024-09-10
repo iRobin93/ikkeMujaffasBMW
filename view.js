6137dd13edf3d9a24a8fe00ea838cc6e1ca1928c
Stian Finjord <stian.finjord@gmail.com>
2024-09-10 13:06:17 +0200

    Lagt til knapper og bil

--- view.js
+++ view.js
@@ -3,17 +3,28 @@ function showView() {
     document.getElementById("app").innerHTML = /*HTML*/ `
         <div>Dette er main view</div>
        
-        <div> ${drawBar(50,"blue")}</div>
+        <div> ${drawBar(kulometer,"blue")}</div>
+        <div>${drawCar()}</div>
+        <button onclick="buyExpensiveUpgrade()">Oppgrader beste kvalitet</button>
+        <button onclick="buyMediumUpgrade()">Oppgrader middels kvalitet</button>
+        <button onclick="buyCheapUpgrade()">Oppgrader dårlig kvalitet</button>
     `
 }
 
 
-function drawBar(percentageFilled, color) {
-    let barColor = 'lightblue';
+function drawBar(percentageFilled) {
  
-    let html = `
+    let html = /*HTML*/ `
         <div class="barContainer">
-            <div class="barContent" style="width: ${percentageFilled}%; background-color: ${barColor}"></div>
+            <div class="barContent" style="width: ${percentageFilled}%"></div>
+        </div>
+    `;
+    return html;
+}
+
+function drawCar() {
+    let html = `
+        <div class="car">
         </div>
     `;
     return html;
