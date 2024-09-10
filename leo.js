065d6afae03fbc006e16c179e51b85ee3ba7606b
Oldskull-one <leobastoss@gmail.com>
2024-09-10 12:36:05 +0200

    Leo.js
    test

--- leo.js
+++ leo.js
@@ -1,12 +1,26 @@
-        //LEO
-        function checkWinCon(){
+
+
+        let kulOMeter = 0;
+
+        function updateKulOMeter(increase) {
+                
+                kulOMeter += increase;
+            
+                if (kulOMeter < 0) kulOMeter = 0;
+                if (kulOMeter > 100) kulOMeter = 100;
 
         }
-        //LEO
-        function updateMoney(int){
+        
 
-        }
-        //LEO
-        function updateKulometer() {
+                //LEO
+                function checkWinCon(){
 
-        }
+                }
+                //LEO
+                function updateMoney(int){
+        
+                }
+                //LEO
+                function updateKulometer() {
+        
+                }
