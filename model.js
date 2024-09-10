1476630a2fd76e8e32308d6db3246793ef8583a7
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2024-09-10 13:17:20 +0200

    robin

--- model.js
+++ model.js
@@ -1,12 +1,31 @@
 let kulometer;
 let penger;
-let kompisArray = [];
-let kompisObject = {};
+let kompisArray = [
+    {
+        Name: "Kompis1",
+        moneyIncrease: 20
+    },
+    {
+        Name: "Kompis2",
+        moneyIncrease: 40
+    }
+];
+
+let showKompis;
+
 let mekanikerObject = {};
 let mekanikere = [];
 let pupUpMessage = "";        
         
         //Robin
-        function getRandomKompis() {
+        setTimeout(getRandomKompis, Math.floor(Math.random()*15000+10000))
 
+        function getRandomKompis() {
+            getRandomNr = Math.floor(Math.random()*kompisArray.length)
+            
+            showKompis = kompisArray[getRandomNr]
+            setTimeout(getRandomKompis, Math.floor(Math.random()*15000+10000))
+            
+
+            hilsKompis(showKompis.Name, showKompis.moneyIncrease);
         }
